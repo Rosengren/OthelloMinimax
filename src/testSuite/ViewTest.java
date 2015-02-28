56e43316e21cce7d5f7258b0c66a29f80abe663d
Rosengren <kev.rosengren@gmail.com>
2015-02-28 08:30:53 -0500

    initial file structure

---
+++ src/testSuite/ViewTest.java
@@ -0,0 +1,17 @@
+package testSuite;
+
+import org.junit.After;
+import org.junit.Before;
+
+public class ViewTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+}
---
+++ src/testSuite/othelloTestSuite.java
@@ -0,0 +1,15 @@
+package testSuite;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+        ControllerTest.class,
+        ModelTest.class,
+        ViewTest.class
+})
+public class othelloTestSuite {
+}
